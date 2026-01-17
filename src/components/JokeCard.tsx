74805de1aba26d4113922a702a10dbce86094f8c
TheEggHealer <TheEggKiller@gmail.com>
2026-01-17 15:36:44 +0100

    Change JokeCard design

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -6,41 +6,24 @@ import { useEffect, useRef, useState } from "react"
 interface JokeCardProps {
   joke: Joke
   created: string
+  seed?: number
 }
 
-function JokeCard({ joke, created }: JokeCardProps) {
-  const [hover, setHover] = useState<boolean>(false)
-  const [titleHeight, setTitleHeight] = useState<number>(0)
-  const [descriptionHeight, setDescriptionHeight] = useState<number>(0)
-  const titleRef = useRef<HTMLHeadingElement>(null)
-  const descriptionRef = useRef<HTMLDivElement>(null)
+function JokeCard({ joke, created, seed }: JokeCardProps) {
 
-  useEffect(() => {
-    if (titleRef.current) {
-      setTitleHeight(titleRef.current.clientHeight)
-    }
-
-    if (descriptionRef.current) {
-      setDescriptionHeight(descriptionRef.current.clientHeight)
-    }
-  })
+  const hash = parseInt((created + seed).replace(/\D/g, ''), 10)
+  const rot = (hash % 4) - 2
 
   return (
-    <Box className="joke-card" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-      <Image h='100%' src={joke.img_url} className="joke-card-image" />
-
-      <Box ref={descriptionRef} className="joke-description" bottom={hover ? 0 : `${-descriptionHeight + titleHeight + 20}px`}>
-        <Stack gap={20}>
-          <Center ref={titleRef} mih={70}>
-            <Title order={3} ta="center" c="white">{joke.title}</Title>
-          </Center>
-            <Text ta="center" c="white">
-            {joke.description.length > 200 
-              ? `${joke.description.substring(0, 200)}...` 
-              : joke.description}
-            </Text>
+    <Box className="joke-card-root" style={{ rotate: `${rot}deg` }}>
+      <Stack>
+        <Image src={joke.img_url} bdrs={5}/>
+        <Stack p={5} gap={10}>
+          <Title order={3}>{joke.title}</Title>
+          <Text>{created}</Text>
+          <Text>{joke.description}</Text>
         </Stack>
-      </Box>
+      </Stack>
     </Box>
   )
 }
--- src/components/JokesFeed.tsx
+++ src/components/JokesFeed.tsx
@@ -1,16 +1,66 @@
-import { Box, Text } from "@mantine/core"
+import { Box, Group, Stack, Text } from "@mantine/core"
 import './JokesFeed.css'
 import { useViewportSize } from "@mantine/hooks";
+import { useEffect, useState, type ReactNode } from "react";
+import { getApprovedJokes } from "../services/firestore";
+import type { Joke } from "../types/types";
+import JokeCard from "./JokeCard";
 
 function JokesFeed() {
+  const [jokes, setJokes] = useState<{created: string, joke: Joke}[]>([])
+  const [columns, setColumns] = useState<ReactNode[]>([])
   const { width } =  useViewportSize()
 
-  const columns = width > 1300 ? 3 : width > 900 ? 2 : 1
+  const columnCount = width > 1300 ? 3 : width > 900 ? 2 : 1
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const document = await getApprovedJokes()
+      if(document) {
+        const jokes = Object.keys(document).map((key) => {
+          return {
+            created: key,
+            joke: document[key]
+          }
+        }) as {created: string, joke: Joke}[]
+
+        setJokes(jokes.concat(jokes).concat(jokes).concat(jokes).concat(jokes).concat(jokes).concat(jokes))
+        console.log('Fetched jokes.')
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    const cols: ReactNode[][] = []
+
+    for(let i = 0; i < columnCount; i++) {
+      cols.push([])
+    }
+
+    jokes.forEach((jokes, index) => {
+      cols[index % columnCount].push((
+        <JokeCard 
+          joke={jokes.joke}
+          created={jokes.created}
+          seed={index * 1283} />
+      ))
+    })
+
+    setColumns(cols.map((children) => (
+      <Stack flex={1} gap={30} justify="start">
+        {children}
+      </Stack>
+    )))
+
+    console.log('Created columns.', cols.length)
+  }, [jokes, width])
+
 
   return (
     <Box flex={1} className="jokes-feed-root">
-      <Text>Width: {width}</Text>
-      <Text>Columns: {columns}</Text>
+      <Group w='100%' gap={30} align="start">{columns}</Group>
     </Box>
   )
 }
